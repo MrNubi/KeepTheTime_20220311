c5fbf0fcc5e3de6337745c2303279d1e506fef4e
neraize <neraize@gmail.com>
2022-03-11 14:49:18 +0900

    로그인 화면 추가

---
+++ app/src/main/java/com/example/keepthetime_20220311/SiginInActivity.kt
@@ -0,0 +1,26 @@
+package com.example.keepthetime_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_20220311.databinding.ActivitySiginInBinding
+
+class SiginInActivity : BaseActivity() {
+
+    lateinit var binding:ActivitySiginInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sigin_in)
+        SetupEvents()
+        setValues()
+    }
+
+    override fun SetupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
