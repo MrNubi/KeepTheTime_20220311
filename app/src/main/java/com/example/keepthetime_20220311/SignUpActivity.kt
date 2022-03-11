c447072036e3376708c03e3bd41e8e81e654bf9c
neraize <neraize@gmail.com>
2022-03-11 17:14:44 +0900

    회원가입 api추가 / 실제 호출

--- app/src/main/java/com/example/keepthetime_20220311/SignUpActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime_20220311
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20220311.databinding.ActivitySignUpBinding
 import com.example.keepthetime_20220311.datas.BasicResponse
@@ -34,6 +35,10 @@ class SignUpActivity : BaseActivity() {
                     call: Call<BasicResponse>,
                     response: Response<BasicResponse>
                 ) {
+                    if(response.isSuccessful){
+                        Toast.makeText(mContext, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
 
                 }
 
