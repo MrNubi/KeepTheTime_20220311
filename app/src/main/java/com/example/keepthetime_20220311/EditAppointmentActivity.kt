a0c9c3dcec7a0d83bd85935a5a167e7739c35aa2
seojungmin <neraize@gmail.com>
2022-03-15 17:10:08 +0900

    하나의 마커를 클릭될대마다좌표만 변경 -지도에 다시 나타나도록 map도 다시 대입

--- app/src/main/java/com/example/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/EditAppointmentActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.DatePicker
 import android.widget.TimePicker
 import android.widget.Toast
@@ -11,6 +12,7 @@ import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,6 +22,11 @@ class EditAppointmentActivity : BaseActivity() {
 
     // 약속시간 일/시를 저장해줄 Calendar (월 값이 0~11 로 움직이게 맞춰져있다.)
     val mSelectedAppointmentDateTime =  Calendar.getInstance() // 기본값 : 현재 일시
+
+    // 약속 장소 관련 멤버변수
+    var marker : Marker? =null  // 지도에 표시될 하나의 마커
+
+    var mSelectedLatLng : LatLng? =null // 약속 장소 위/경도도 처음에는 설정하지 않은 상태
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -104,6 +111,22 @@ class EditAppointmentActivity : BaseActivity() {
             // coord에 설정한 좌표로 -> 네이버지도의 카메라 이동
             val cameraUpdate = CameraUpdate.scrollTo(coord)
             naverMap.moveCamera(cameraUpdate)
+
+            // 첫 마커 좌표 -> 학원위치
+//            val marker = Marker()  => 멤버변수로 하나의 마커만 만들어서 관리하자
+            marker =Marker()
+            marker!!.position = coord
+            marker!!.map =naverMap
+
+            // 지도 클릭 이벤트
+            naverMap.setOnMapClickListener { pointF, latLng ->
+//                Log.d("클릭된 위/경도", "위도:${latLng.latitude},  경도:${latLng.longitude}")
+
+                // 클릭될때 마다 마커 새로 추가
+
+                marker!!.position = latLng
+                marker!!.map = naverMap
+            }
 
         }
     }
