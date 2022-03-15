4d8edac411456cb736160b745987f60cf3876cdd
seojungmin <neraize@gmail.com>
2022-03-15 14:47:41 +0900

    뷰페이져의 페이지이동 -> 바텀 네비게이션의 선택메뉴 선정 - 뷰페이저2 /바텀 네비게이션 연동

--- app/src/main/java/com/example/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.example.keepthetime_20220311.adapters.MainViewPager2Adapter
 import com.example.keepthetime_20220311.adapters.MainViewPagerAdapter
 import com.example.keepthetime_20220311.databinding.ActivityMainBinding
@@ -40,7 +41,18 @@ class MainActivity : BaseActivity() {
             return@setOnItemSelectedListener true
         }
         // 뷰페이지의 페이지 이동 -> 바텀 네비게이션의 메뉴 선택
+        binding.mainViewPager2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
 
+            // 추상메소드 x .  이벤트처리 함수를 직접 오버라이딩해야함
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                binding.mainBottomNav.selectedItemId = when(position){
+                    0-> R.id.myAppointment
+                    else-> R.id.myProfile
+                }
+            }
+        })
     }
 
     override fun setValues() {
