3341114a1befb56124449f779209a23e8728d531
seojungmin <neraize@gmail.com>
2022-03-16 11:01:34 +0900

    일시 파싱 관련 주석 추가

--- app/src/main/java/com/example/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
@@ -28,6 +28,12 @@ class AppointmentRecyclerAdapter(
 
             txtTitle.text = data.title
             txtPlaceName.text =data.place
+
+            // 서버가 주는 dateTime (String -2022-03-15 10:57:23 양식)
+
+            // 중간에 Calendar / Date 형태로 변환 => 파싱할때부터 Calendar / Date라고 하면?
+            
+            // 출력하고 싶은 dateTime (String - 22년 3월 15일 오후 2시 30분 양식)
         }
     }
 
