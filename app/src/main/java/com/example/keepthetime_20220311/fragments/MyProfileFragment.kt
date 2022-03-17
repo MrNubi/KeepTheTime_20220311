b6a8b2a414689328b65d004bd1c159243b07b222
neraize <neraize@gmail.com>
2022-03-17 09:48:55 +0900

    내 출발 장소 목록 관리 화면 추가

--- app/src/main/java/com/example/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/example/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -11,6 +11,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.keepthetime_20220311.ManageMyFriendsActivity
+import com.example.keepthetime_20220311.ManagePlaceActivity
 import com.example.keepthetime_20220311.R
 import com.example.keepthetime_20220311.SplashActivity
 import com.example.keepthetime_20220311.databinding.FragmentMyprofileBinding
@@ -42,6 +43,13 @@ class MyProfileFragment :BaseFragment(){
     }
 
     override fun setupEvents() {
+
+        binding.btnManagePlace.setOnClickListener{
+
+            val myIntent = Intent(mContext, ManagePlaceActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
         binding.btnManageMyFriends.setOnClickListener {
 
