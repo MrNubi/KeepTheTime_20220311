4928500e3f6b2216543b1514d14f081c35116bd9
neraize <neraize@gmail.com>
2022-03-17 16:40:25 +0900

    돌아온 토큰값으로, 페북서버에 다시 내 정보 조회 요청 - 받아온 정보를 로그로 확인

--- app/src/main/java/com/example/keepthetime_20220311/SiginInActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SiginInActivity.kt
@@ -11,10 +11,7 @@ import com.example.keepthetime_20220311.api.ServerAPI
 import com.example.keepthetime_20220311.databinding.ActivitySiginInBinding
 import com.example.keepthetime_20220311.datas.BasicResponse
 import com.example.keepthetime_20220311.utils.ContextUtil
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.kakao.sdk.user.UserApiClient
@@ -50,11 +47,18 @@ class SiginInActivity : BaseActivity() {
                     // 페북 로그인성공 -> 페북 서버의 토큰값 받기
                     Log.d("페북로그인성공", result?.accessToken.toString())
 
-                    // 받은 토큰으로  -> 내 정보도 받아오자
+                    // 받은 토큰으로  -> 내 정보도 받아오자 => GraphRequest 클래스 활용
 
                     // 1. 정보를 받아오면 뭘 할건지?  인터페이스 설정
+                    val graphRequest = GraphRequest.newMeRequest(result?.accessToken, object :GraphRequest.GraphJSONObjectCallback{
+                        override fun onCompleted(jsonObj: JSONObject?, response: GraphResponse?) {
+
+                            Log.d("받아온 정보", jsonObj!!.toString())
+                        }
+                    })
 
                     // 2. 실제로 요청 호출
+                    graphRequest.executeAsync()
 
                 }
 
