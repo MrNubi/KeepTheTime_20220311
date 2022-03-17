0affb9e0c52fd19d58c80ab65974f9ab1c1fd17d
neraize <neraize@gmail.com>
2022-03-17 12:01:00 +0900

    실제 데이터 반영 -리스트뷰와 동일한 사용법 적용

--- app/src/main/java/com/example/keepthetime_20220311/adapters/StartPlaceSpinnerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_20220311/adapters/StartPlaceSpinnerAdapter.kt
@@ -28,6 +28,10 @@ class StartPlaceSpinnerAdapter(
 
         val data = mList[position]
 
+        val txtStartPlaceName = row.findViewById<TextView>(R.id.txtStartPlaceName)
+
+        txtStartPlaceName.text =data.name
+
         return row
     }
 }
